ef5521f6c7d4145f4ad78fd9e8bf0535b4492fb6
jr1412594 <jr.medina1412@gmail.com>
2021-08-16 15:16:20 -0600

    first commit

---
+++ primitiveDataTypes.js
@@ -0,0 +1,39 @@
+// let age = 23;
+// let age2 = 23.0;
+
+// console.log(typeof(age))
+// console.log(typeof(age2), "this is 2")
+// console.log(age === age2)
+
+//seven primitive data types
+
+// Number floating point numbers ==> used for decimals and integers
+//String sequence of characters ==> used for text
+// Boolean logical type true or false ==> used for taking decisions
+//Undefined value taken by a variable that is not yet defined ('empty value')
+//Null also means empty value
+//Symbol values that cannot be changed
+//BigInt Larger integers that the number type can hold
+
+
+/* JavaScript has dynamic typing:
+    We do Not have to manually define the data type
+    of the value stored in a variable. Instead, data types
+    are determined automatically
+*/
+
+
+true;
+
+// console.log(javaScriptIsFun)
+let javaScriptIsFun = true;
+// console.log(javaScriptIsFun)
+
+// console.log(typeof javaScriptIsFun, 'original')
+
+
+javaScriptIsFun = 'New Value'
+// console.log(typeof javaScriptIsFun, 'dynamic typing')
+
+console.log(typeof null)
+console.log(typeof undefined)
