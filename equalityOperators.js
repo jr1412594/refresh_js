1d6de1655ad8ca78138a7bc76307e5aafe2c4312
jr1412594 <jr.medina1412@gmail.com>
2021-10-06 17:36:48 -0600

    strict and loose equals

---
+++ equalityOperators.js
@@ -0,0 +1,16 @@
+const age = '18';
+
+if (age === 18) console.log('you an adult(strict)')
+
+if (age == 18) console.log('you an adult (loose)')
+
+/// === strict equals
+// does not perform type corecion
+
+// == loose equals
+// '18' == 18 will be true
+// loose equals will convert string to number
+
+
+
+
