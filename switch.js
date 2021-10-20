958bb7501c2ac9203a070c40675013bc4d3cb804
jr1412594 <jr.medina1412@gmail.com>
2021-10-20 17:50:11 -0600

    switch case

---
+++ switch.js
@@ -0,0 +1,46 @@
+// const day = 'monday';
+// const day = 'tuesday';
+const day = 'wednesday';
+// const day = 'thursday';
+// const day = 'friday';
+// const day = 'saturday';
+// const day = 'sunday';
+// const day = 'nun\'ya'
+
+// switch(day) { // if (day === 'monday')
+//     case 'monday':
+//         console.log('Plan course structure');
+//         console.log("Go to coding meetup");
+//         break;
+//     case 'tuesday':
+//         console.log('Prepare theory videos');
+//         break;
+//     case 'wednesday':
+//     case 'thursday':
+//         console.log('Write code examples');
+//         break;
+//     case 'friday':
+//         console.log('Record videos');
+//         break;
+//     case 'saturday':
+//     case 'sunday':
+//         console.log('Enjoy the weekend');
+//         break;
+//     default:
+//         console.log('not a valid day!');
+// }
+
+if (day === 'monday') {
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+    console.log('Prepare theory videos');
+} else if (day === 'wednesday' || day === 'thursday') {
+    console.log('Write code examples'); 
+} else if (day === 'friday') {
+    console.log('Record videos'); 
+} else if (day === 'saturday' || day == 'sunday') {
+    console.log('Enjoy the weekend');
+} else {
+    console.log('Not a valid day');
+}
